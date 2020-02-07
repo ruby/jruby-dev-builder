549c0716004323ec26726ba54e9acfb2df646a05
Benoit Daloze <eregontp@gmail.com>
2020-02-07 11:22:53 +0100

    Add script to find the latest JRuby snapshot build

---
+++ find-jruby-head-url.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+require 'nokogiri'
+
+base_url = 'https://oss.sonatype.org/content/repositories/snapshots/org/jruby/jruby-dist'
+index_url = "#{base_url}/maven-metadata.xml"
+
+STDERR.puts index_url
+xml = URI.open(index_url, &:read)
+STDERR.puts xml
+
+versions = Nokogiri::XML(xml).css('version').map(&:text)
+most_recent = (versions - %w[9000.dev-SNAPSHOT]).last
+
+builds_url = "#{base_url}/#{most_recent}/maven-metadata.xml"
+STDERR.puts builds_url
+xml = URI.open(builds_url, &:read)
+STDERR.puts xml
+
+last_build = Nokogiri::XML(xml).css('snapshotVersion').select { |node|
+  classifier = node.at('classifier')
+  classifier and classifier.text == 'bin' and node.at('extension').text == 'tar.gz'
+}.map { |node| node.at('value').text }.last
+
+final_url = "#{base_url}/#{most_recent}/jruby-dist-#{last_build}-bin.tar.gz"
+puts final_url
