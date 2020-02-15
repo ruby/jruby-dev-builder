fae5d84879c471b312952f6576c4d804d591325d
Benoit Daloze <eregontp@gmail.com>
2020-02-15 17:59:52 +0100

    Create a version without needing Nokogiri
    * Which means no dependencies are needed to run this script.

--- find-jruby-head-url.rb
+++ find-jruby-head-url.rb
@@ -1,5 +1,60 @@
 require 'open-uri'
-require 'nokogiri'
+
+module MicroXMLParser
+  HEADER = %r{<\?xml\s.+\?>\n}
+  ATTRIBUTE = %r{(\w+)="([^"]+)"}
+  OPEN_TAG = %r{<(\w+)((?:\s+#{ATTRIBUTE})*)>}
+  CLOSE_TAG = %r{</(\w+)>}
+  TEXT = %r{([^<]*)}
+
+  def self.parse(xml)
+    xml = xml.strip
+    raise xml unless xml.start_with? HEADER
+    xml = xml[$&.size..-1].strip
+    value, rest = rec(xml)
+    raise rest unless rest.empty?
+    value
+  end
+
+  def self.rec(xml)
+    xml = xml.strip
+    if xml.start_with? OPEN_TAG
+      entries = []
+      while xml.start_with? OPEN_TAG
+        tag, attributes = $1, $2
+        xml = xml[$&.size..-1].strip
+
+        until attributes.empty?
+          attributes = attributes.strip
+          raise attributes unless attributes.start_with? ATTRIBUTE
+          key, value = $1, $2
+          attributes = attributes[$&.size..-1]
+          entries << { key.to_sym => value }
+        end
+
+        value, xml = rec(xml)
+        entries << { tag.to_sym => value }
+
+        close_tag = "</#{tag}>"
+        raise "Missing close tag #{close_tag}: #{xml}" unless xml.start_with? close_tag
+        xml = xml[close_tag.size..-1].strip
+      end
+
+      keys = entries.map { |entry| entry.first.first }
+      if keys == keys.uniq
+        entries = entries.inject({}, :merge)
+      end
+
+      [entries, xml]
+    elsif xml.start_with? TEXT
+      text = $1
+      xml = xml[$&.size..-1]
+      [text, xml]
+    else
+      raise xml
+    end
+  end
+end
 
 base_url = 'https://oss.sonatype.org/content/repositories/snapshots/org/jruby/jruby-dist'
 index_url = "#{base_url}/maven-metadata.xml"
@@ -8,7 +63,8 @@ STDERR.puts index_url
 xml = URI.open(index_url, &:read)
 STDERR.puts xml
 
-versions = Nokogiri::XML(xml).css('version').map(&:text)
+parsed = MicroXMLParser.parse(xml)
+versions = parsed.dig(:metadata, :versioning, :versions).map { |e| e[:version] }
 most_recent = (versions - %w[9000.dev-SNAPSHOT]).last
 
 builds_url = "#{base_url}/#{most_recent}/maven-metadata.xml"
@@ -16,10 +72,10 @@ STDERR.puts builds_url
 xml = URI.open(builds_url, &:read)
 STDERR.puts xml
 
-last_build = Nokogiri::XML(xml).css('snapshotVersion').select { |node|
-  classifier = node.at('classifier')
-  classifier and classifier.text == 'bin' and node.at('extension').text == 'tar.gz'
-}.map { |node| node.at('value').text }.last
+parsed = MicroXMLParser.parse(xml)
+last_build = parsed.dig(:metadata, :versioning, :snapshotVersions).map { |e| e[:snapshotVersion] }.select { |node|
+  node[:classifier] == 'bin' and node[:extension] == 'tar.gz'
+}.map { |node| node[:value] }.last
 
 final_url = "#{base_url}/#{most_recent}/jruby-dist-#{last_build}-bin.tar.gz"
 puts final_url
